a468eed7a7b602cccb5da4521da663114596b28c
Rupam Bora <iamrupam99@gmail.com>
2022-12-01 07:47:24 +0530

    Day 2 Completed

---
+++ flutter_catalog/lib/home_page.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+  final int nums = 30;
+  final String name = 'Jeng!';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Catalog App'),
+      ),
+      body: Center(
+        child: Container(
+          child: Text('Welcome to $nums $name case...'),
+        ),
+      ),
+    );
+  }
+}
